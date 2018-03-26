f3e28c572a3f4025f44274f50e15e9e5fb5a922a
asteriasandippal <sandip.pal@asteria.co.in>
2018-03-26 14:58:57 +0530

    Added Arrow Function

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
+const defaultLine = "Start your journey now!";
+const BR = "<br />";
+const print = (...args) => console.log(args[0] + ": " + args[1]);
+
+const option = {
+    __intro: "You can type",
+    __options: ["left", "right", "up", "down", "help"],
+    outputOptions() {
+        this.__options.forEach(f => addToOutput(this.__intro + " " + f));
+    }
+
+}
+
 function main() {
-    const element = document.querySelector("#enter");
+    let element = document.querySelector("#enter");
     element.addEventListener("click", onClickEnter, false);
 }
 
 function addToOutput(newLine="Start your journey now!") {
-    const output = document.querySelector("#output");
+    let output = document.querySelector("#output");
 
     output.innerHTML = output.innerHTML + "<br />" + newLine;
 }
 
 function onClickEnter() {
-    const commands = document.querySelector("#commands");
-
-    addToOutput(commands.value);
+    let commands = document.querySelector("#commands");
+    if (commands.value === "help") {
+        option.outputOptions();
+    } else {
+        addToOutput(commands.value);
+    }
 }
 
 main();
+
+print("log", "hello");
